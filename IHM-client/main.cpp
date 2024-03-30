2def6759c726d05e7f6e894ea87c9948fa9b0c59
ahliko <killian.guillemot@ynov.com>
2024-03-30 15:37:49 +0100

    Update

--- IHM-client/main.cpp
+++ IHM-client/main.cpp
@@ -1,16 +1,48 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <grpcpp/grpcpp.h>
+#include "com.grpc.pb.h"
 
-int main(int argc, char *argv[])
-{
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using ::HelloRequest;
+using ::HelloResponse;
+using ::Greeter;
+
+class GreeterServiceImpl final : public Greeter::Service {
+    Status SayHello(ServerContext *context, const HelloRequest *request, HelloResponse *response) override {
+        std::string prefix("Hello, ");
+        response->set_message(prefix + request->name());
+        return Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string server_address("0.0.0.0:50051");
+    GreeterServiceImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
+}
+
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
+    for (const QString &locale: uiLanguages) {
         const QString baseName = "IHM_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
             a.installTranslator(&translator);
@@ -19,5 +51,6 @@ int main(int argc, char *argv[])
     }
     MainWindow w;
     w.show();
+    RunServer();
     return a.exec();
 }
