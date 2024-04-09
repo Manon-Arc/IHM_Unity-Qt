fb6c4c2b7501f0b8f18abb761238d895ea242fb2
ahliko <killian.guillemot@ynov.com>
2024-04-09 10:51:07 +0200

    Update

--- IHM-client/mainwindow.cpp
+++ IHM-client/mainwindow.cpp
@@ -1,7 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <memory>
-#include <QPushButton>
 #include <QVBoxLayout>
 
 
@@ -10,22 +9,35 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     m_grpcClient = new GrpcServiceClient(grpc::CreateChannel("10.33.73.161:50051", grpc::InsecureChannelCredentials()));
 
-    auto *button_up = ui->Top;
-    auto *button_down = ui->Bottom;
-    auto *button_left = ui->Left;
-    auto *button_right = ui->Right;
+    m_button_up = ui->Top;
+    m_button_down = ui->Bottom;
+    m_button_left = ui->Left;
+    m_button_right = ui->Right;
 
-    QVBoxLayout *layout = new QVBoxLayout(this);
+    m_slider = ui->CamSlider;
+    m_labelValue = ui->SliderValue;
 
-    auto *slider = ui->CamSlider;
+    m_button_open = ui->Open;
+    m_button_close = ui->Close;
+    m_button_x1 = ui->X1;
+    m_button_x2 = ui->x2;
+    m_button_x1_2 = ui->X1_2;
+    m_button_x2_2 = ui->x2_2;
+    m_button_cube = ui->Cube;
+    m_button_sphere = ui->Sphere;
+    m_button_cylinder = ui->Cylindre;
 
-    layout->addWidget(slider);
 
-    connect(button_up, &QPushButton::clicked, this, &MainWindow::moveUp);
-    connect(button_down, &QPushButton::clicked, this, &MainWindow::moveDown);
-    connect(button_left, &QPushButton::clicked, this, &MainWindow::moveLeft);
-    connect(button_right, &QPushButton::clicked, this, &MainWindow::moveRight);
-    connect(slider, &QSlider::valueChanged, this, &MainWindow::moveCam);
+    connect(m_button_up, &QPushButton::clicked, this, &MainWindow::moveUp);
+    connect(m_button_down, &QPushButton::clicked, this, &MainWindow::moveDown);
+    connect(m_button_left, &QPushButton::clicked, this, &MainWindow::moveLeft);
+    connect(m_button_right, &QPushButton::clicked, this, &MainWindow::moveRight);
+    connect(m_slider, &QSlider::valueChanged, this, &MainWindow::moveCam);
+    connect(m_button_cube, &QPushButton::clicked, this, &MainWindow::createCube);
+    connect(m_button_sphere, &QPushButton::clicked, this, &MainWindow::createSphere);
+    connect(m_button_cylinder, &QPushButton::clicked, this, &MainWindow::createCylinder);
+    connect(m_button_open, &QPushButton::clicked, this, &MainWindow::openTongs);
+    connect(m_button_close, &QPushButton::clicked, this, &MainWindow::closeTongs);
 }
 
 MainWindow::~MainWindow() {
@@ -55,5 +67,31 @@ void MainWindow::moveRight() {
 
 void MainWindow::moveCam(int value) {
     m_grpcClient->MoveCam(value);
+    m_labelValue->setText(QString::number(value));
     qDebug("Déplacement de la caméra");
+}
+
+void MainWindow::createCube() {
+    m_grpcClient->InstanceObject("cube");
+    qDebug() << "Création d'un cube";
+}
+
+void MainWindow::createSphere() {
+    m_grpcClient->InstanceObject("sphere");
+    qDebug() << "Création d'une sphère";
+}
+
+void MainWindow::createCylinder() {
+    m_grpcClient->InstanceObject("cylinder");
+    qDebug() << "Création d'un cylindre";
+}
+
+void MainWindow::openTongs() {
+    m_grpcClient->TongsManageOpening(true);
+    qDebug() << "Ouverture des pinces";
+}
+
+void MainWindow::closeTongs() {
+    m_grpcClient->TongsManageOpening(false);
+    qDebug() << "Fermeture des pinces";
 }
